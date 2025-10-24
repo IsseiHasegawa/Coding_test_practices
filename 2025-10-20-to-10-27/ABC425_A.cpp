fa2e6e62c94cefe43f77bd298657881426579b00
IsseiHasegawa <hasegawa01@allegheny.edu>
2025-10-24 17:43:00 -0400

    Done ABC425_A

---
+++ 2025-10-20-to-10-27/ABC425_A.cpp
@@ -0,0 +1,25 @@
+/*
+問題文
+正整数 N が与えられます。
+i=1,2,…,N について (−1)^i×i^3 を計算したときの、
+それら N 個の値の総和を求めてください。
+
+
+制約
+N は 1 以上 10 以下の整数
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    int sum = 0;
+    for (int i = 1; i <= N; i++){
+        sum += int(pow(-1, i)) * int(pow(i, 3));
+    }
+
+    cout << sum << endl;
+}
