025474c77e8ec3ad652af60797ad78ddcf9a0cd3
IsseiHasegawa <hasegawa01@allegheny.edu>
2025-11-09 18:02:33 -0500

    Done ABC423_B(second attempt)

---
+++ 2025-11-3-to-11-9/ABC423_B_redo.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+
+    int L = n;
+    int R = 0;
+    for (int i = 0; i < n; i++){
+        if (v[i] == 1){
+            L = min(L, i);
+            R = max(R, i);
+        }
+    }
+
+    if (L == n){
+        cout << 0 << endl;
+        return 0;
+    }
+
+    cout << R - L << endl;
+    return 0;
+}
