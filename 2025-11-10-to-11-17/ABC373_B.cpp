578e1ced0f51310018ccd14f5a071b3db19ddf77
IsseiHasegawa <hasegawa01@allegheny.edu>
2025-11-10 13:01:36 -0500

    Done ABC373_B

---
+++ 2025-11-10-to-11-17/ABC373_B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    map<char, int> pos;
+    for (int i = 0; i < 26; i++){
+        pos[s[i]] = i;
+    }
+    int ans = 0;
+    for (int i = 0; i < 25; i++){
+        char from = 'A' + i;
+        char to = from + 1;
+        ans += abs(pos[from]-pos[to]);
+    }
+
+    cout << ans << endl;
+
+}
