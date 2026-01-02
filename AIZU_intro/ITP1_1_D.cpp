41f2454a20df6e22da128bc766db1b490d6eaa30
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2026-01-02 16:53:13 -0600

    Done IPT1_1_D'

---
+++ AIZU_intro/ITP1_1_D.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int h = n / (60*60);
+    int m = (n % (60*60)) / 60;
+    int s = (n % (60*60)) % 60;
+
+    cout << h << ':' << m << ':' << s << endl;
+
+    return 0;
+}
