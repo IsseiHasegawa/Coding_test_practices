b54b4e81727c26e7f72cbf297948209304cd7853
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2026-01-15 17:26:52 -0500

    Improve ALDS1_1_A

--- AIZU_intro/ALDS1_1_A.cpp
+++ AIZU_intro/ALDS1_1_A.cpp
@@ -1,42 +1,41 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-    vector<int> v(n);
+void showArr(int arr[], int n){
     for (int i = 0; i < n; i++){
-        cin >> v[i];
-    }
-    
-    for (int j = 0; j < n; j++){
-        if (j != n-1){
-            cout << v[j] << " ";
+        if (i != n-1){
+            cout << arr[i] << " ";
         } else {
-            cout << v[j] << endl;;
-
+            cout << arr[i] << endl;;
         }
     }
+}
 
+void insertSort(int arr[], int n){
     for (int i = 1; i < n; i++){
-        int a = v[i];
-        int b = i - 1;
-        while (b >= 0 && v[b] > a){
-            v[b+1] = v[b];
-            b--;
-        }
-        v[b+1] = a;
+        int v = arr[i];
+        int j = i - 1;
 
-        for (int j = 0; j < n; j++){
-            if (j != n-1){
-                cout << v[j] << " ";
-            } else {
-              cout << v[j] << endl;;
-
-            }
+        while (j >= 0 && v < arr[j]){
+            arr[j+1] = arr[j];
+            j--;
         }
+        arr[j+1] = v;
+        showArr(arr, n);
     }
+}
+
+int main(){
+    int n; 
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    showArr(arr, n);
+    insertSort(arr, n);
     return 0;
 }
