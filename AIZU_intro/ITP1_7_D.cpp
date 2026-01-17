cae97a4af6d5df983c9c402796bfddf5ef73cbd0
issei <issei@isseis-Mac-mini.local>
2026-01-17 13:01:48 -0500

    push

--- AIZU_intro/ITP1_7_D.cpp
+++ AIZU_intro/ITP1_7_D.cpp
@@ -4,5 +4,41 @@
 using namespace std;
 
 int main(){
+    int n, m, l;
+    cin >> n >> m >> l;
+
+    vector<vector<int>> v1(n, vector<int>(m, 0));
+    vector<vector<int>> v2(m, vector<int>(l, 0));
+    vector<vector<int>> newV(n, vector<int>(l, 0));
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            cin >> v1[i][j];
+        }
+    }
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < l; j++){
+            cin >> v2[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < l; j++){
+            int sum = 0;
+            for (int k = 0; k < m; k++){
+                sum += v1[i][k] * v2[k][j];
+            }
+            newV[i][j] = sum;
+        }
+    }
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < l; j++){
+            cout << newV[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
