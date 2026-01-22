063b38a0a90e1b25488fb820ef76fbf343208ec7
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2026-01-21 23:13:35 -0500

    Valid_parentheses

--- LeetCode/Add_two_numbers.cpp
+++ LeetCode/Add_two_numbers.cpp
@@ -30,7 +30,20 @@ public:
                 sum += l1->val;
                 l1 = l1->next;
             }
+
+            if (l2 != nullptr){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            int digit = sum % 10;
+
+            current->next = new ListNode(digit);
+            current = current->next;
         }
+
+        return dummy.next;
 
         
     }
